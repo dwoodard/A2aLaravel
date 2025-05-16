1f63817be9c69073e6c986bc831db6186de6e80b
Dustin Woodard <dustin.woodard@gmail.com>
2025-05-15 22:18:49 -0600

    feat: Reorganize A2A routes and add configuration for agent capabilities and authentication

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,26 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 abstract class TestCase extends OrchestraTestCase
 {
     // You can add package-specific setup here if needed
+
+    protected function defineDatabaseMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Dwoodard\A2aLaravel\A2aLaravelServiceProvider::class,
+        ];
+    }
 }
